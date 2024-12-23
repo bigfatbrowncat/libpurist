79a1eed6618e589c53112f148c09bda9de8850a9
Ilya Mizus <bigfatbrowncat@gmail.com>
2024-12-23 16:42:56 +0200

    Added asan to the debug Skia build. Removed .git folder from the vscode files watcher

--- third-party/skia-world/build-linux.debug.sh
+++ third-party/skia-world/build-linux.debug.sh
@@ -1,4 +1,4 @@
 export OUT=out/static-debug
-export ARGS='is_official_build=false skia_use_egl=true skia_use_x11=false skia_use_fontconfig=true'
+export ARGS='is_official_build=false skia_use_egl=true skia_use_x11=false skia_use_fontconfig=true extra_cflags_cc=["-fsanitize=address,undefined"]'
 
 ./build-linux.sh

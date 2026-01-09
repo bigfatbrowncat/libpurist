3dc6c3f59882d4632774546b85b77baa1a3c367a
Ilya Mizus <bigfatbrowncat@gmail.com>
2026-01-09 14:30:12 +0000

    Moved lru_cache to separate file

--- examples/term_skia/term_skia.cpp
+++ examples/term_skia/term_skia.cpp
@@ -1,4 +1,5 @@
 #include "TermSubprocess.h"
+#include "lru_cache.h"
 
 // libpurist headers
 #include <purist/graphics/skia/DisplayContentsSkia.h>
@@ -30,12 +31,7 @@
 #include <vterm.h>
 #include <vterm_keycodes.h>
 
-// System headers
-#include <termios.h>
-#include <unistd.h>
-#include <sys/time.h>
-
-// std headers
+// C++ std headers
 #include <map>
 #include <memory>
 #include <iostream>
@@ -58,59 +54,6 @@ namespace pg = purist::graphics;
 namespace pi = purist::input;
 namespace pgs = purist::graphics::skia;
 
-template<typename key_t, typename value_t>
-class lru_cache {
-public:
-    typedef typename std::pair<key_t, value_t> key_value_pair_t;
-    typedef typename std::list<key_value_pair_t>::iterator list_iterator_t;
-
-    lru_cache(size_t max_size) :
-        _max_size(max_size) {
-    }
-    
-    std::optional<key_value_pair_t> put(const key_t& key, const value_t& value) {
-        std::optional<key_value_pair_t> res = std::nullopt;
-        auto it = _cache_items_map.find(key);
-        _cache_items_list.push_front(key_value_pair_t(key, value));
-        if (it != _cache_items_map.end()) {
-            _cache_items_list.erase(it->second);
-            _cache_items_map.erase(it);
-        }
-        _cache_items_map[key] = _cache_items_list.begin();
-        
-        if (_cache_items_map.size() > _max_size) {
-            auto last = _cache_items_list.end();
-            last--;
-            _cache_items_map.erase(last->first);
-            res = std::make_optional(_cache_items_list.back());
-            _cache_items_list.pop_back();
-        }
-        return res;
-    }
-    
-    const value_t& get(const key_t& key) {
-        auto it = _cache_items_map.find(key);
-        if (it == _cache_items_map.end()) {
-            throw std::range_error("There is no such key in cache");
-        } else {
-            _cache_items_list.splice(_cache_items_list.begin(), _cache_items_list, it->second);
-            return it->second->second;
-        }
-    }
-    
-    bool exists(const key_t& key) const {
-        return _cache_items_map.find(key) != _cache_items_map.end();
-    }
-    
-    size_t size() const {
-        return _cache_items_map.size();
-    }
-    
-private:
-    std::list<key_value_pair_t> _cache_items_list;
-    std::map<key_t, list_iterator_t> _cache_items_map;
-    size_t _max_size;
-};
 
 template <typename T> class Matrix {
     T* buf;
